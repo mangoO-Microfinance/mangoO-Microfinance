842681a4f5999cdeab2dd1d2a98530c16c27ef55
mangoO-Microfinance <mangoOmicrofinance@gmail.com>
2016-04-09 13:18:13 +0300

    Database updates

--- database/mangoo_fresh.sql
+++ database/mangoo_fresh.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 23. Mrz 2016 um 12:25
+-- Erstellungszeit: 09. Apr 2016 um 12:17
 -- Server Version: 5.6.21
 -- PHP-Version: 5.6.3
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `custmarried` (
 `custmarried_id` int(11) NOT NULL,
   `custmarried_status` varchar(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `custmarried`
@@ -39,7 +39,8 @@ INSERT INTO `custmarried` (`custmarried_id`, `custmarried_status`) VALUES
 (0, 'N/A'),
 (1, 'Single'),
 (2, 'Married'),
-(3, 'Widowed');
+(3, 'Widowed'),
+(4, 'Divorced');
 
 -- --------------------------------------------------------
 
@@ -49,25 +50,25 @@ INSERT INTO `custmarried` (`custmarried_id`, `custmarried_status`) VALUES
 
 CREATE TABLE IF NOT EXISTS `customer` (
 `cust_id` int(11) NOT NULL,
-  `cust_no` varchar(20) DEFAULT NULL,
-  `cust_name` varchar(75) DEFAULT NULL,
-  `cust_dob` int(15) DEFAULT NULL,
-  `custsex_id` int(1) DEFAULT NULL,
+  `cust_no` varchar(50) DEFAULT NULL,
+  `cust_name` varchar(100) DEFAULT NULL,
+  `cust_dob` int(11) DEFAULT NULL,
+  `custsex_id` int(11) DEFAULT NULL,
   `cust_address` varchar(100) DEFAULT NULL,
   `cust_phone` varchar(50) DEFAULT NULL,
-  `cust_email` varchar(50) DEFAULT NULL,
+  `cust_email` varchar(100) DEFAULT NULL,
   `cust_occup` varchar(50) DEFAULT NULL,
-  `custmarried_id` int(5) DEFAULT NULL,
-  `cust_heir` varchar(50) DEFAULT NULL,
-  `cust_heirrel` varchar(25) DEFAULT NULL,
+  `custmarried_id` int(11) DEFAULT NULL,
+  `cust_heir` varchar(100) DEFAULT NULL,
+  `cust_heirrel` varchar(50) DEFAULT NULL,
   `cust_lengthres` int(11) DEFAULT NULL,
-  `cust_since` int(15) DEFAULT NULL,
-  `custsick_id` int(8) DEFAULT NULL,
-  `cust_lastsub` int(15) DEFAULT NULL,
-  `cust_active` int(1) DEFAULT NULL,
+  `cust_since` int(11) DEFAULT NULL,
+  `custsick_id` int(11) DEFAULT NULL,
+  `cust_lastsub` int(11) DEFAULT NULL,
+  `cust_active` int(1) NOT NULL DEFAULT '0',
   `cust_lastupd` int(11) DEFAULT NULL,
   `cust_pic` varchar(100) DEFAULT NULL,
-  `user_id` int(6) DEFAULT NULL
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -75,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `customer` (
 --
 
 INSERT INTO `customer` (`cust_id`, `cust_no`, `cust_name`, `cust_dob`, `custsex_id`, `cust_address`, `cust_phone`, `cust_email`, `cust_occup`, `custmarried_id`, `cust_heir`, `cust_heirrel`, `cust_lengthres`, `cust_since`, `custsick_id`, `cust_lastsub`, `cust_active`, `cust_lastupd`, `cust_pic`, `user_id`) VALUES
-(0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+(0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 0, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -160,7 +161,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
   `emplsex_id` int(11) NOT NULL DEFAULT '1',
   `emplmarried_id` int(11) NOT NULL,
   `empl_position` varchar(100) DEFAULT NULL,
-  `empl_salary` int(11) NOT NULL,
+  `empl_salary` int(11) DEFAULT NULL,
   `empl_address` varchar(100) DEFAULT NULL,
   `empl_phone` varchar(50) DEFAULT NULL,
   `empl_email` varchar(100) DEFAULT NULL,
@@ -177,7 +178,7 @@ CREATE TABLE IF NOT EXISTS `employee` (
 --
 
 INSERT INTO `employee` (`empl_id`, `empl_no`, `empl_name`, `empl_dob`, `emplsex_id`, `emplmarried_id`, `empl_position`, `empl_salary`, `empl_address`, `empl_phone`, `empl_email`, `empl_in`, `empl_out`, `empl_lastupd`, `empl_active`, `empl_pic`, `user_id`) VALUES
-(0, '0', '0', 0, 0, 0, NULL, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 1);
+(0, '0', '0', 0, 0, 0, '', 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -226,33 +227,34 @@ CREATE TABLE IF NOT EXISTS `expenses` (
 
 CREATE TABLE IF NOT EXISTS `exptype` (
 `exptype_id` int(11) NOT NULL,
-  `exptype_type` varchar(50) NOT NULL
+  `exptype_type` varchar(50) NOT NULL,
+  `exptype_short` varchar(8) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `exptype`
 --
 
-INSERT INTO `exptype` (`exptype_id`, `exptype_type`) VALUES
-(1, 'Airtime'),
-(2, 'Bank Charges'),
-(3, 'Committee Welfare'),
-(4, 'Electricity'),
-(5, 'Gifts'),
-(6, 'Internet'),
-(7, 'IT'),
-(8, 'Motorcycle'),
-(9, 'Office Space'),
-(10, 'Petty Cash'),
-(11, 'Rent'),
-(12, 'Staff Facilitation'),
-(13, 'Staff Welfare'),
-(14, 'Stationary'),
-(15, 'Tax'),
-(16, 'Transport'),
-(17, 'Insurance'),
-(18, 'Annual Share Dividend'),
-(19, 'Annual Savings Interest');
+INSERT INTO `exptype` (`exptype_id`, `exptype_type`, `exptype_short`) VALUES
+(1, 'Airtime', 'EXP_AIT'),
+(2, 'Bank Charges', 'EXP_BCH'),
+(3, 'Committee Welfare', 'EXP_COW'),
+(4, 'Electricity', 'EXP_ELC'),
+(5, 'Gifts', 'EXP_GFT'),
+(6, 'Internet', 'EXP_ITN'),
+(7, 'IT', 'EXP_ITC'),
+(8, 'Motorcycle', 'EXP_MOT'),
+(9, 'Office Space', 'EXP_OFF'),
+(10, 'Petty Cash', 'EXP_PCA'),
+(11, 'Rent', 'EXP_RNT'),
+(12, 'Staff Facilitation', 'EXP_SFC'),
+(13, 'Staff Welfare', 'EXP_SWF'),
+(14, 'Stationary', 'EXP_STN'),
+(15, 'Tax', 'EXP_TAX'),
+(16, 'Transport', 'EXP_TRN'),
+(17, 'Insurance', 'EXP_INS'),
+(18, 'Annual Share Dividend', 'EXP_SHD'),
+(19, 'Annual Savings Interest', 'EXP_INT');
 
 -- --------------------------------------------------------
 
@@ -263,7 +265,7 @@ INSERT INTO `exptype` (`exptype_id`, `exptype_type`) VALUES
 CREATE TABLE IF NOT EXISTS `fees` (
 `fee_id` int(11) NOT NULL,
   `fee_name` varchar(50) NOT NULL,
-  `fee_short` varchar(5) NOT NULL,
+  `fee_short` varchar(8) NOT NULL,
   `fee_value` float NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
@@ -272,14 +274,14 @@ CREATE TABLE IF NOT EXISTS `fees` (
 --
 
 INSERT INTO `fees` (`fee_id`, `fee_name`, `fee_short`, `fee_value`) VALUES
-(1, 'Entry Fee', 'EF', 10000),
-(2, 'Withdrawal Fee', 'WF', 1000),
-(3, 'Stationary Sales', 'StS', 2000),
-(4, 'Anual Subscription', 'ASub', 5000),
-(5, 'Loan Fee', 'LF', 1),
-(6, 'Loan Application Fee', 'LAF', 10000),
-(7, 'Loan Fine', 'LFine', 15000),
-(8, 'Loan Interest Rate', 'LIR', 4);
+(1, 'Entry Fee', 'FEE_ENT', 10000),
+(2, 'Withdrawal Fee', 'FEE_WDL', 1000),
+(3, 'Stationary Sales', 'FEE_STS', 2000),
+(4, 'Anual Subscription', 'FEE_ASB', 5000),
+(5, 'Loan Fee', 'FEE_LOF', 1),
+(6, 'Loan Application Fee', 'FEE_LAP', 10000),
+(7, 'Loan Default Fine', 'FEE_LDF', 15000),
+(8, 'Loan Interest Rate', 'FEE_LIR', 3);
 
 -- --------------------------------------------------------
 
@@ -289,8 +291,11 @@ INSERT INTO `fees` (`fee_id`, `fee_name`, `fee_short`, `fee_value`) VALUES
 
 CREATE TABLE IF NOT EXISTS `incomes` (
 `inc_id` int(11) NOT NULL,
-  `cust_id` int(11) NOT NULL,
   `inctype_id` int(11) NOT NULL,
+  `cust_id` int(11) DEFAULT NULL,
+  `loan_id` int(11) DEFAULT NULL,
+  `ltrans_id` int(11) DEFAULT NULL,
+  `sav_id` int(11) DEFAULT NULL,
   `inc_amount` int(11) NOT NULL,
   `inc_date` int(15) NOT NULL,
   `inc_receipt` int(11) NOT NULL,
@@ -307,24 +312,25 @@ CREATE TABLE IF NOT EXISTS `incomes` (
 
 CREATE TABLE IF NOT EXISTS `inctype` (
 `inctype_id` int(11) NOT NULL,
-  `inctype_type` varchar(50) NOT NULL
+  `inctype_type` varchar(50) NOT NULL,
+  `inctype_short` varchar(8) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `inctype`
 --
 
-INSERT INTO `inctype` (`inctype_id`, `inctype_type`) VALUES
-(1, 'Entrance Fee'),
-(2, 'Withdrawal Fee'),
-(3, 'Loan Fee'),
-(4, 'Interest'),
-(5, 'Loan Default Fines'),
-(6, 'Stationary Sales'),
-(7, 'Loan Application Fee'),
-(8, 'Subscription Fee'),
-(9, 'Other'),
-(10, 'Insurance');
+INSERT INTO `inctype` (`inctype_id`, `inctype_type`, `inctype_short`) VALUES
+(1, 'Entrance Fee', 'INC_ENF'),
+(2, 'Withdrawal Fee', 'INC_WDF'),
+(3, 'Loan Fee', 'INC_LOF'),
+(4, 'Loan Interest', 'INC_INT'),
+(5, 'Loan Default Fine', 'INC_LDF'),
+(6, 'Stationary Sales', 'INC_STS'),
+(7, 'Loan Application Fee', 'INC_LAF'),
+(8, 'Subscription Fee', 'INC_SUF'),
+(9, 'Other', 'INC_OTH'),
+(10, 'Insurance', 'INC_INS');
 
 -- --------------------------------------------------------
 
@@ -342,7 +348,6 @@ CREATE TABLE IF NOT EXISTS `loans` (
   `loan_issued` int(2) NOT NULL,
   `loan_principal` int(11) NOT NULL,
   `loan_interest` float NOT NULL,
-  `cur_id` int(11) DEFAULT NULL,
   `loan_appfee_receipt` int(11) NOT NULL,
   `loan_fee` int(11) NOT NULL,
   `loan_fee_receipt` int(11) NOT NULL,
@@ -370,7 +375,7 @@ CREATE TABLE IF NOT EXISTS `loans` (
 CREATE TABLE IF NOT EXISTS `loanstatus` (
 `loanstatus_id` int(11) NOT NULL,
   `loanstatus_status` varchar(50) NOT NULL,
-  `loanstatus_short` varchar(5) NOT NULL
+  `loanstatus_short` varchar(8) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
@@ -378,11 +383,11 @@ CREATE TABLE IF NOT EXISTS `loanstatus` (
 --
 
 INSERT INTO `loanstatus` (`loanstatus_id`, `loanstatus_status`, `loanstatus_short`) VALUES
-(1, 'Pending', 'PEN'),
-(2, 'Approved', 'APP'),
-(3, 'Refused', 'REF'),
-(4, 'Abandoned', 'ABN'),
-(5, 'Cleared', 'CLR');
+(1, 'Pending', 'LST_PEN'),
+(2, 'Approved', 'LST_APP'),
+(3, 'Refused', 'LST_REF'),
+(4, 'Abandoned', 'LST_ABN'),
+(5, 'Cleared', 'LST_CLR');
 
 -- --------------------------------------------------------
 
@@ -442,11 +447,12 @@ CREATE TABLE IF NOT EXISTS `savbalance` (
 
 CREATE TABLE IF NOT EXISTS `savings` (
 `sav_id` int(11) NOT NULL,
+  `savtype_id` int(11) NOT NULL,
+  `sav_mother` int(11) DEFAULT NULL,
   `cust_id` int(11) NOT NULL,
+  `ltrans_id` int(11) DEFAULT NULL,
   `sav_date` int(15) NOT NULL,
   `sav_amount` int(15) NOT NULL DEFAULT '0',
-  `cur_id` int(11) NOT NULL,
-  `savtype_id` int(11) NOT NULL,
   `sav_receipt` int(11) DEFAULT NULL,
   `sav_slip` int(10) NOT NULL,
   `sav_created` int(15) DEFAULT NULL,
@@ -462,7 +468,7 @@ CREATE TABLE IF NOT EXISTS `savings` (
 CREATE TABLE IF NOT EXISTS `savtype` (
 `savtype_id` int(11) NOT NULL,
   `savtype_type` varchar(20) NOT NULL,
-  `savtype_short` varchar(3) NOT NULL
+  `savtype_short` varchar(8) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
@@ -470,15 +476,15 @@ CREATE TABLE IF NOT EXISTS `savtype` (
 --
 
 INSERT INTO `savtype` (`savtype_id`, `savtype_type`, `savtype_short`) VALUES
-(1, 'Deposit', 'D'),
-(2, 'Withdrawal', 'W'),
-(3, 'Savings Interest', 'I'),
-(4, 'W/drawal Fee', 'F_W'),
-(5, 'Subscription Fee', 'F_S'),
-(6, 'Loan Default Fine', 'LDF'),
-(7, 'Loan Fee', 'F_L'),
-(8, 'Loan Repayment', 'LR'),
-(9, 'Share Dividend', 'SHD');
+(1, 'Deposit', 'SAV_DEP'),
+(2, 'Withdrawal', 'SAV_WDL'),
+(3, 'Savings Interest', 'SAV_INT'),
+(4, 'W/drawal Fee', 'SAV_WDF'),
+(5, 'Subscription Fee', 'SAV_SUF'),
+(6, 'Loan Default Fine', 'SAV_LDF'),
+(7, 'Loan Fee', 'SAV_LOF'),
+(8, 'Loan Repayment', 'SAV_LRP'),
+(9, 'Share Dividend', 'SAV_SHD');
 
 -- --------------------------------------------------------
 
@@ -515,18 +521,18 @@ CREATE TABLE IF NOT EXISTS `settings` (
 --
 
 INSERT INTO `settings` (`set_id`, `set_name`, `set_short`, `set_value`) VALUES
-(1, 'Minimum Savings Balance', 'MSB', '10000'),
-(2, 'Minimum Loan Principal', 'MinLP', '500000'),
-(3, 'Maximum Loan Principal', 'MaxLP', '10000000'),
-(4, 'Currency Abbreviation', 'CUR', 'UGX'),
-(5, 'Auto-fine Defaulters', 'AUF', ''),
-(6, 'Account Deactivation', 'DEACT', ''),
-(7, 'Dashboard Left', 'DashL', 'dashboard/dash_subscr.php'),
-(8, 'Dashboard Right', 'DashR', 'dashboard/dash_loandefaults.php'),
-(9, 'Interest Calculation', 'IntCALC', 'modules/mod_inter_float.php'),
-(10, 'Guarantor Limit', 'MaxGuar', '3'),
-(11, 'Minimum Membership', 'MinMemb', '4'),
-(12, 'Maximum Principal-Savings Ratio', 'MaxPSR', '500');
+(1, 'Minimum Savings Balance', 'SET_MSB', '10000'),
+(2, 'Minimum Loan Principal', 'SET_MLP', '500000'),
+(3, 'Maximum Loan Principal', 'SET_XLP', '10000000'),
+(4, 'Currency Abbreviation', 'SET_CUR', 'UGX'),
+(5, 'Auto-fine Defaulters', 'SET_AUF', ''),
+(6, 'Account Deactivation', 'SET_DEA', ''),
+(7, 'Dashboard Left', 'SET_DBL', 'dashboard/dash_stat_cust.php'),
+(8, 'Dashboard Right', 'SET_DBR', 'dashboard/dash_none.php'),
+(9, 'Interest Calculation', 'SET_ICL', 'modules/mod_inter_fixed.php'),
+(10, 'Guarantor Limit', 'SET_GUA', '3'),
+(11, 'Minimum Membership', 'SET_MEM', '6'),
+(12, 'Maximum Principal-Savings Ratio', 'SET_PSR', '500');
 
 -- --------------------------------------------------------
 
@@ -572,14 +578,17 @@ CREATE TABLE IF NOT EXISTS `ugroup` (
   `ugroup_delete` int(2) NOT NULL,
   `ugroup_report` int(11) NOT NULL,
   `ugroup_created` int(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `ugroup`
 --
 
 INSERT INTO `ugroup` (`ugroup_id`, `ugroup_name`, `ugroup_admin`, `ugroup_delete`, `ugroup_report`, `ugroup_created`) VALUES
-(1, 'Administrator', 1, 1, 1, 0);
+(1, 'Administrator', 1, 1, 1, 1453123220),
+(2, 'Management', 0, 1, 1, 1453144125),
+(3, 'Employee', 0, 1, 0, 1453125729),
+(4, 'Ext-Admin', 1, 0, 0, 1453123276);
 
 -- --------------------------------------------------------
 
@@ -592,8 +601,16 @@ CREATE TABLE IF NOT EXISTS `user` (
   `user_name` varchar(200) NOT NULL,
   `user_pw` varchar(255) NOT NULL,
   `ugroup_id` int(11) NOT NULL,
+  `empl_id` int(11) NOT NULL,
   `user_created` int(15) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_name`, `user_pw`, `ugroup_id`, `empl_id`, `user_created`) VALUES
+(0, '', '', 0, 0, 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -757,7 +774,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT für Tabelle `custmarried`
 --
 ALTER TABLE `custmarried`
-MODIFY `custmarried_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+MODIFY `custmarried_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT für Tabelle `customer`
 --
@@ -872,9 +889,12 @@ MODIFY `shareval_id` int(11) NOT NULL AUTO_INCREMENT;
 -- AUTO_INCREMENT für Tabelle `ugroup`
 --
 ALTER TABLE `ugroup`
-MODIFY `ugroup_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `ugroup_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
 MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
