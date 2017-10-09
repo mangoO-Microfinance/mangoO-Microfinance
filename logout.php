be58acaf74aa6634eb776382a33e278e2381a9ec
mangoO <mangoOmicrofinance@gmail.com>
2017-10-09 20:30:33 +0200

    login.php and logout.php updated to reorganised functions checkSQL() and sanitize().

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@
 	$logrec_end = time();
 	$sql_update_logrec = "UPDATE logrec SET logrec_end = '$logrec_end', logrec_logout = '1' WHERE logrec_id = '$_SESSION[logrec_id]'";
 	$query_update_logrec = mysqli_query($db_link, $sql_update_logrec);
-	checkSQL($query_update_logrec, $db_link);
+	checkSQL($db_link, $query_update_logrec);
 
 	logout();
 ?>
