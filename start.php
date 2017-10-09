56e68cd422ac62ac1e475423ccc71ac715a354bc
mangoO <mangoOmicrofinance@gmail.com>
2017-10-09 18:40:03 +0200

    start.php migrated

--- start.php
+++ start.php
@@ -2,9 +2,9 @@
 <?PHP
 	require 'functions.php';
 	checkLogin();
-	connect();
-	getSettings();
-	getFees();
+	$db_link = connect();
+	getSettings($db_link);
+	getFees($db_link);
 ?>
 
 <html>
@@ -15,7 +15,7 @@
 	<!-- HTML BODY -->
 	<body>
 		<!-- MENU -->
-		<?PHP 
+		<?PHP
 				includeMenu(1);
 		?>
 		<!-- MENU MAIN -->
@@ -24,7 +24,7 @@
 			<a href="cust_new.php">New Customer</a>
 			<a href="loan_search.php">Search Loan</a>
 		</div>
-		
+
 		<!-- Left Side of Dashboard -->
 		<div class="content_left" style="width:50%;">
 			<?PHP include $_SESSION['set_dashl']; ?>
@@ -34,9 +34,9 @@
 		<div  class="content_right" style="width:50%;">
 			<?PHP include $_SESSION['set_dashr']; ?>
 		</div>
-		
+
 		<!-- Logout Reminder Message -->
 		<?PHP	checkLogout();	?>
-		
+
 	</body>
 </html>
