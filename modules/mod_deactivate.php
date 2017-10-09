3d28c7ec67092aebbf40435c0b4be5f368a3fefe
mangoO <mangoOmicrofinance@gmail.com>
2017-10-09 23:14:50 +0200

    Module files migrated

--- modules/mod_deactivate.php
+++ modules/mod_deactivate.php
@@ -1,13 +1,14 @@
 <?PHP
 
-//Calculating grace period before deactivation in months 
+//Calculating grace period before deactivation in months
 $timestamp = time();
 $graceperiod = convertDays(365) + convertMonths($_SESSION['set_deact']);
 
 //Set customer to "inactive" after grace period for subscription payment expired
 if ($row_subscrdef['cust_lastsub'] < ($timestamp - $graceperiod) && $row_subscrdef['cust_active'] == 1){
 	$sql_deactivate = "UPDATE customer SET cust_active = '0' WHERE cust_id = '$row_subscrdef[cust_id]'";
-	$query_deactiate = mysql_query($sql_deactivate);
+	$query_deactiate = mysqli_query($db_link, $sql_deactivate);
+	checkSQL($db_link, $query_deactiate);
 	showMessage('The membership for '.$row_subscrdef['cust_name'].' ('.$row_subscrdef['cust_id'].') has been set to INACTIVE.');
 }
 
--- modules/mod_subscr.php
+++ modules/mod_subscr.php
@@ -1,5 +1,5 @@
 <?PHP
-if (($result_cust['cust_lastsub']+convertDays(365)) < $timestamp ){ 
+if (($result_cust['cust_lastsub']+convertDays(365)) < $timestamp ){
 	$visibility = 'block';
 	if($result_cust['cust_active'] == 1) showMessage('This Customer needs to renew his / her Subscription!');
 }
@@ -7,46 +7,47 @@ else $visibility = 'none';
 
 //RENEW-MEMBERSHIP-Button
 if(isset($_POST['subscr_renew'])){
-	
+
 	//Sanitize user input
-	$subscr_date = strtotime(sanitize($_POST['subscr_date']));
-	$subscr_receipt = sanitize($_POST['subscr_receipt']);
-	$subscr_from_sav = sanitize($_POST['subscr_from_sav']);
+	$subscr_date = strtotime(sanitize($db_link, $_POST['subscr_date']));
+	$subscr_receipt = sanitize($db_link, $_POST['subscr_receipt']);
+	$subscr_from_sav = sanitize($db_link, $_POST['subscr_from_sav']);
 	$timestamp = time();
 	$sav_id = array();
-	
+
 	// Get Subscription Fee
-	getFees();
-	
+	getFees($db_link);
+
 	// Insert Subscription Fee into SAVINGS if applicable
 	if ($subscr_from_sav == 1){
 		$fee_subscr_sav = $_SESSION['fee_subscr'] * (-1);
-		
+
 		$sql_insert_fee = "INSERT INTO savings (savtype_id, cust_id, sav_date, sav_amount, sav_receipt, sav_created, user_id) VALUES ('5', '$_SESSION[cust_id]', '$subscr_date', '$fee_subscr_sav', '$subscr_receipt', '$timestamp', '$_SESSION[log_id]')";
-		$query_insert_fee = mysql_query ($sql_insert_fee);
-		checkSQL($query_insert_fee);
-		
+		$query_insert_fee = mysqli_query($db_link, $sql_insert_fee);
+		checkSQL($db_link, $query_insert_fee);
+
 		// Update savings account balance
-		updateSavingsBalance($_SESSION['cust_id']);
-		
+		updateSavingsBalance($db_link, $_SESSION['cust_id']);
+
 		// Get SAV_ID for the latest entry
 		$sql_savid = "SELECT MAX(sav_id) FROM savings WHERE cust_id = '$_SESSION[cust_id]' AND sav_receipt = '$subscr_receipt' AND sav_created = '$timestamp'";
-		$query_savid = mysql_query($sql_savid);
-		checkSQL($query_savid);
-		$sav_id = mysql_fetch_row($query_savid);
+		$query_savid = mysqli_query($db_link, $sql_savid);
+		checkSQL($db_link, $query_savid);
+		$sav_id = mysqli_fetch_row($query_savid);
 	}
 	else $sav_id[0] = NULL;
-		
+
 	// Insert Subscription Fee into INCOMES
 	$sql_insert_fee = "INSERT INTO incomes (cust_id, inctype_id, sav_id, inc_amount, inc_date, inc_receipt, inc_created, user_id) VALUES ('$_SESSION[cust_id]', '8', '$sav_id[0]', '$_SESSION[fee_subscr]', '$subscr_date', '$subscr_receipt', '$timestamp', '$_SESSION[log_id]')";
-	$query_insert_fee = mysql_query ($sql_insert_fee);
-	checkSQL($query_insert_fee);
-	
+	$query_insert_fee = mysqli_query($db_link, $sql_insert_fee);
+	checkSQL($db_link, $query_insert_fee);
+
 	//Set Customer to ACTIVE and update SUBSCRIPTION DATE
-	$query_active = mysql_query("UPDATE customer SET cust_lastsub = '$subscr_date', cust_active = '1' WHERE cust_id = '$_SESSION[cust_id]'");
-	checkSQL($query_active);
-	
-	//header('Location: customer.php?cust='.$_SESSION['cust_id']);
+	$sql_active = "UPDATE customer SET cust_lastsub = '$subscr_date', cust_active = '1' WHERE cust_id = '$_SESSION[cust_id]'";
+	$query_active = mysqli_query($db_link, $sql_active);
+	checkSQL($db_link, $query_active);
+
+	header('Location: customer.php?cust='.$_SESSION['cust_id']);
 }
 ?>
 
