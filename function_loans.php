b52b98b0abd50e5044d83f6c09175c771f22011b
mangoO <mangoOmicrofinance@gmail.com>
2017-10-10 12:00:35 +0200

    Revert "function_loans.php"
    This reverts commit e85864f2b3f77051a8a64601d3166fb54c78946a.

--- function_loans.php
+++ function_loans.php
@@ -2,33 +2,33 @@
 /**
 	*	Update Loan Status
 	*/
-	function updateStatus($db_link){
+	function updateStatus(){
 	}
-
+	
 /**
 	* Charge Loan Default Fine
 	*/
-	function chargeFine($db_link, $loanID, $fine_amount, $fine_receipt, $fine_date, $fine_sav, $logID){
+	function chargeFine($loanID, $fine_amount, $fine_receipt, $fine_date, $fine_sav, $logID){
 	}
 
 /**
 	* Make Loan Repayment
 	*/
-	function makeRepayment($db_link){
+	function makeRepayment(){
 	}
-
+	
 /**
 	* Re-calculate due payments
 	*/
-	function updateInterFloat($db_link, $loanID, $principal, $interRate){
-
+	function updateInterFloat($loanID, $principal, $interRate){
+		
 		$sql_instalments = "SELECT ltrans_id FROM ltrans WHERE ltrans_date IS NULL AND ltrans.loan_id = '$loanID'";
-		$query_instalments = mysqli_query($db_link, $sql_instalments);
-		checkSQL($db_link, $query_instalments);
+		$query_instalments = mysql_query($sql_instalments);
+		checkSQL($query_instalments);
 		$instalments = array();
-		while($row_inst = mysqli_fetch_assoc($query_instalments)) $instalments [] = $row_inst;
+		while($row_inst = mysql_fetch_assoc($query_instalments)) $instalments [] = $row_inst;
 		$instalCount = count($instalments);
-
+		
 		// Re-calculate monthly rate
 		$pDue = round($principal / $instalCount, -3);
 
@@ -38,26 +38,26 @@
 		// Re-calculate due payments and update LTRANS
 		$ltrans_pDue = $pDue;
 		$ltrans_pRemain = $principal;
-
+		
 		$i = 1;
 		foreach($instalments as $inst){
-
+			
 			$ltrans_iDue = ($ltrans_pRemain / 100 * $interRate);
-
-			//Add differences on principal on first iteration.
+			
+			//Add differences on principal on first iteration. 
 			if ($i == 1) $ltrans_pDue = $ltrans_pDue + $difference_principal;
-
+			
 			//Update LTRANS
 			$sql_update_ltrans = "UPDATE ltrans SET ltrans_principaldue = '$ltrans_pDue', ltrans_interestdue = '$ltrans_iDue' WHERE ltrans_id = '$inst[ltrans_id]'";
-			$query_update_ltrans = mysqli_query($db_link, $sql_update_ltrans);
-			checkSQL($db_link, $query_update_ltrans);
-
-			//Reduce remaining principal by paid amount
+			$query_update_ltrans = mysql_query ($sql_update_ltrans);
+			checkSQL($query_update_ltrans);
+			
+			//Reduce remaining principal by paid amount 
 			$ltrans_pRemain = $ltrans_pRemain - $ltrans_pDue;
-
+			
 			//Reset principal amount to default value after first iteration
 			if ($i == 1) $ltrans_pDue = $pDue;
-
+			
 			$i++;
 		}
 
